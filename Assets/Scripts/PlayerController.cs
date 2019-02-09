af85f82e0f8adbc0a673f0aae26796f6a2bd8a96
Jessebeau2001 <jessegamen@gmail.com>
2019-02-09 19:59:03 +0100

    Sprite Update
    Removed old blue, red and green sprite and replaced it with a 16-bit version which is now able to change color with the '1, 2 and 3' key. added floaty animation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,40 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public float FloatStrength;
+    public float hoverSpeed;
 
     private Animator anim;
+    private Vector3 floatY;
 
-    // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float moveVertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        transform.Translate(new Vector3(moveHorizontal, moveVertical));
+        transform.Translate(new Vector3(moveHorizontal, moveVertical + floatY.y));
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown("1"))
+        {
+            GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            GetComponent<SpriteRenderer>().color = new Color32(255, 66, 66, 255);
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            GetComponent<SpriteRenderer>().color = new Color32(0, 255, 0, 255);
+        }
+
+        floatY.y = (Mathf.Sin(hoverSpeed * Time.time) * FloatStrength);
     }
 }
