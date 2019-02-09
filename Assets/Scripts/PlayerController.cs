1ae87cf5e0d8f2b578388a7c024e69a94334078e
Jessebeau2001 <jessegamen@gmail.com>
2019-02-09 16:21:02 +0100

    BasicMovement
    added some animaitons and basic movement for the basic blu player character.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed;
 
+    private Animator anim;
+
     // Use this for initialization
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,5 +22,8 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Translate(new Vector3(moveHorizontal, moveVertical));
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
     }
 }
