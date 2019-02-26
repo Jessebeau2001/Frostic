44c65c02c16339f7a722eb0265f05f1ce8f3de79
Jessebeau2001 <jessegamen@gmail.com>
2019-02-26 18:03:50 +0100

    Fully working point item system
    The system was made using a switch statement. All the item haveb een turned into prefabs so is is expandable.
    
    TO DO:
    Make a score display after play time is over.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+
+    private PointSys pt; //reference to the PoinSys script
     void Start()
     {
+        pt = GameObject.FindGameObjectWithTag("PointSystem").GetComponent<PointSys>();
     }
 
     void Update()
@@ -16,5 +19,30 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Translate(new Vector3(moveHorizontal, moveVertical));
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        switch (col.gameObject.tag){
+            case "Coin":
+                Destroy(col.gameObject);
+                pt.points += 50;
+                break;
+            case "Gem":
+                Destroy(col.gameObject);
+                pt.points += 100;
+                break;
+            case "Money_Bag":
+                Destroy(col.gameObject);
+                pt.points += 75;
+                break;
+            case "Treasure_Chest":
+                Destroy(col.gameObject);
+                pt.points += 125;
+                break;
+            case "Gold_Cluster":
+                Destroy(col.gameObject);
+                pt.points += 75;
+                break;
+        }
     }
 }
